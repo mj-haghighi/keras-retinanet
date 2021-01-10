60430c99aa38432c8b24e235dc802485ecf3fce9
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-01-11 02:39:48 +0330

    Implement 'scsv_generator' to personalization 'csv_generator'

---
+++ keras_retinanet/preprocessing/scsv_generator.py
@@ -0,0 +1,310 @@
+"""
+Copyright 2017-2018 yhenon (https://github.com/yhenon/)
+Copyright 2017-2018 Fizyr (https://fizyr.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from .generator import Generator
+from ..utils.image import read_image_bgr
+
+import numpy as np
+from PIL import Image
+from six import raise_from
+
+import csv
+import sys
+import os.path
+from collections import OrderedDict
+
+
+def _parse(value, function, fmt):
+    """
+    Parse a string into a value, and format a nice ValueError if it fails.
+
+    Returns `function(value)`.
+    Any `ValueError` raised is catched and a new `ValueError` is raised
+    with message `fmt.format(e)`, where `e` is the caught `ValueError`.
+    """
+    try:
+        return function(value)
+    except ValueError as e:
+        raise_from(ValueError(fmt.format(e)), None)
+
+
+def _read_classes(csv_reader):
+    """ Parse the classes file given by csv_reader.
+    """
+    result = OrderedDict()
+    for line, row in enumerate(csv_reader):
+        line += 1
+
+        try:
+            class_name, class_id = row
+        except ValueError:
+            raise_from(ValueError('line {}: format should be \'class_name,class_id\''.format(line)), None)
+        class_id = _parse(class_id, int, 'line {}: malformed class ID: {{}}'.format(line))
+
+        if class_name in result:
+            raise ValueError('line {}: duplicate class name: \'{}\''.format(line, class_name))
+        result[class_name] = class_id
+    return result
+
+
+def _read_annotations(csv_reader, classes):
+    """ Read annotations from the csv_reader.
+    """
+    result = OrderedDict()
+    for line, row in enumerate(csv_reader):
+        line += 1
+
+        try:
+            img_file, x, y, alpha, class_name = row[:5]
+        except ValueError:
+            raise_from(ValueError('line {}: format should be \'img_file,x,y,alpha,class_name\' or \'img_file,,,,\''.format(line)), None)
+
+        if img_file not in result:
+            result[img_file] = []
+
+        # If a row contains only an image path, it's an image without annotations.
+        if (x, y, alpha, class_name) == ('', '', '', '', ''):
+            continue
+
+        x = _parse(x, float, 'line {}: malformed x: {{}}'.format(line))
+        y = _parse(y, float, 'line {}: malformed y: {{}}'.format(line))
+        alpha = _parse(alpha, float, 'line {}: malformed alpha: {{}}'.format(line))
+
+        
+        # check if the current class name is correctly present
+        if class_name not in classes:
+            raise ValueError('line {}: unknown class name: \'{}\' (classes: {})'.format(line, class_name, classes))
+
+        result[img_file].append({'x': x, 'y': y, 'alpha': alpha, 'class': class_name})
+    return result
+
+
+def _open_for_csv(path):
+    """ Open a file with flags suitable for csv.reader.
+
+    This is different for python2 it means with mode 'rb',
+    for python3 this means 'r' with "universal newlines".
+    """
+    if sys.version_info[0] < 3:
+        return open(path, 'rb')
+    else:
+        return open(path, 'r', newline='')
+
+
+class SCSVGenerator(Generator):
+    """ Generate data for a custom CSV dataset
+        including centers and angles not bounding boxes
+
+    See https://github.com/fizyr/keras-retinanet#csv-datasets for more information.
+    """
+
+    def __init__(
+        self,
+        csv_data_file,
+        csv_class_file,
+        base_dir=None,
+        **kwargs
+    ):
+        """ Initialize a CSV data generator.
+
+        Args
+            csv_data_file: Path to the CSV annotations file. NOTE: You need to have all anotations in one file.
+            csv_class_file: Path to the CSV classes file. NOTE: You need to have all classes in one file.
+            base_dir: Directory w.r.t. where the files are to be searched (defaults to the directory containing the csv_data_file).
+        """
+        self.image_names = []
+        self.image_data  = {}
+        self.base_dir    = base_dir
+
+        # Take base_dir from annotations file if not explicitly specified.
+        if self.base_dir is None:
+            self.base_dir = os.path.dirname(csv_data_file)
+
+        # parse the provided class file
+        try:
+            with _open_for_csv(csv_class_file) as file:
+                self.classes = _read_classes(csv.reader(file, delimiter=','))
+        except ValueError as e:
+            raise_from(ValueError('invalid CSV class file: {}: {}'.format(csv_class_file, e)), None)
+
+        self.labels = {}
+        for key, value in self.classes.items():
+            self.labels[value] = key
+
+        # csv with img_path, x, y, alpha, class_name
+        try:
+            with _open_for_csv(csv_data_file) as file:
+                self.image_data = _read_annotations(csv.reader(file, delimiter=','), self.classes)
+        except ValueError as e:
+            raise_from(ValueError('invalid CSV annotations file: {}: {}'.format(csv_data_file, e)), None)
+        self.image_names = list(self.image_data.keys())
+
+        super(SCSVGenerator, self).__init__(**kwargs)
+
+    def size(self):
+        """ Size of the dataset.
+        """
+        return len(self.image_names)
+
+    def num_classes(self):
+        """ Number of classes in the dataset.
+        """
+        return max(self.classes.values()) + 1
+
+    def has_label(self, label):
+        """ Return True if label is a known label.
+        """
+        return label in self.labels
+
+    def has_name(self, name):
+        """ Returns True if name is a known class.
+        """
+        return name in self.classes
+
+    def name_to_label(self, name):
+        """ Map name to label.
+        """
+        return self.classes[name]
+
+    def label_to_name(self, label):
+        """ Map label to name.
+        """
+        return self.labels[label]
+
+    def image_path(self, image_index):
+        """ Returns the image path for image_index.
+        """
+        return os.path.join(self.base_dir, self.image_names[image_index])
+
+    def image_aspect_ratio(self, image_index):
+        """ Compute the aspect ratio for an image with image_index.
+        """
+        # PIL is fast for metadata
+        image = Image.open(self.image_path(image_index))
+        return float(image.width) / float(image.height)
+
+    def load_image(self, image_index):
+        """ Load an image at the image_index.
+        """
+        return read_image_bgr(self.image_path(image_index))
+
+    def load_annotations(self, image_index):
+        """ Load annotations for an image_index.
+        """
+        path        = self.image_names[image_index]
+        annotations = {'labels': np.empty((0,)), 'center_alpha': np.empty((0, 3))}
+
+        for idx, annot in enumerate(self.image_data[path]):
+            annotations['labels'] = np.concatenate((annotations['labels'], [self.name_to_label(annot['class'])]))
+            annotations['center_alpha'] = np.concatenate((annotations['center_alpha'], [[
+                float(annot['x']),
+                float(annot['y']),
+                float(annot['alpha']),
+            ]]))
+
+        return annotations
+    
+    
+    def filter_annotations(self, image_group, annotations_group, group):
+        """ Filter annotations by removing those that are outside of the image bounds or whose width/height < 0.
+        """
+        # test all annotations
+        for index, (image, annotations) in enumerate(zip(image_group, annotations_group)):
+            # test  x < 0 | y < 0 | x >= image.shape[1] | y >= image.shape[0]
+            invalid_indices = np.where(
+                (annotations['center_alpha'][:, 0] < 0) |
+                (annotations['center_alpha'][:, 1] < 0) |
+                (annotations['center_alpha'][:, 0] > image.shape[1]) |
+                (annotations['center_alpha'][:, 1] > image.shape[0])
+            )[0]
+
+            # delete invalid indices
+            if len(invalid_indices):
+                warnings.warn('Image {} with id {} (shape {}) contains the following invalid boxes: {}.'.format(
+                    self.image_path(group[index]),
+                    group[index],
+                    image.shape,
+                    annotations['center_alpha'][invalid_indices, :]
+                ))
+                for k in annotations_group[index].keys():
+                    annotations_group[index][k] = np.delete(annotations[k], invalid_indices, axis=0)
+        return image_group, annotations_group
+
+    # OVERRIDED on generator`s one
+    def preprocess_group_entry(self, image, annotations):
+        """ Preprocess image and its annotations.
+        """
+        # resize image
+        image, image_scale = self.resize_image(image)
+
+        # preprocess the image
+        image = self.preprocess_image(image)
+
+        # apply resizing to annotations too. NOTE: third parameter is alpha, don`t touch it :) 
+        annotations['center_alpha'][:, :2] *= image_scale
+
+        # convert to the wanted keras floatx
+        image = keras.backend.cast_to_floatx(image)
+
+        return image, annotations
+
+    # OVERRIDED on generator`s one
+    def compute_targets(self, image_group, annotations_group):
+        """ Compute target outputs for the network using images and their annotations.
+        """
+        # get the max image shape
+        max_shape = tuple(max(image.shape[x] for image in image_group) for x in range(3))
+        anchors   = self.generate_anchors(max_shape)
+
+        batches = self.compute_anchor_targets(
+            anchors,
+            image_group,
+            annotations_group,
+            self.num_classes()
+        )
+
+        return list(batches)
+
+
+
+    # OVERRIDED on generator`s one
+    def compute_input_output(self, group):
+        """ Compute inputs and target outputs for the network.
+        """
+        # load images and annotations
+        image_group       = self.load_image_group(group)
+        annotations_group = self.load_annotations_group(group)
+
+        # check validity of annotations
+        image_group, annotations_group = self.filter_annotations(image_group, annotations_group, group)
+
+        # randomly apply visual effect
+        image_group, annotations_group = self.random_visual_effect_group(image_group, annotations_group)
+
+        # randomly transform data, NOTE: bikhial for now :)
+        # image_group, annotations_group = self.random_transform_group(image_group, annotations_group)
+
+        # perform preprocessing steps
+        image_group, annotations_group = self.preprocess_group(image_group, annotations_group)
+
+        # compute network inputs
+        inputs = self.compute_inputs(image_group)
+
+        # compute network targets
+        targets = self.compute_targets(image_group, annotations_group)
+
+        return inputs, targets
