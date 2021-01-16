6e8247498324816ae5b265b456eb57234fcf10b4
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-01-16 16:20:55 +0330

    Bugfix :(

--- keras_retinanet/models/saffronnet.py
+++ keras_retinanet/models/saffronnet.py
@@ -218,8 +218,8 @@ def saffronnet(
             num_anchors             = num_anchors,
             backbone_feature_size   = backbone_feature_size)
         
-        regression = backbone_output(submodels['regression']) 
-        classification = backbone_output(submodels['classification']) 
+        regression = submodels['regression'](backbone_output)
+        classification = submodels['classification'](backbone_output) 
 
     return keras.models.Model(inputs=inputs, outputs=[regression, classifcation], name=name)
 
