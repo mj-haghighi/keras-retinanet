68d0e28aad7d2524b148b62602923246a92de119
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-01-23 10:29:49 +0330

    Add 'RegressLines' and 'ClipLines' in layers

--- keras_retinanet/layers/__init__.py
+++ keras_retinanet/layers/__init__.py
@@ -1,2 +1,2 @@
-from ._smisc import RegressBoxes, UpsampleLike, Anchors, ClipBoxes  # noqa: F401
+from ._smisc import RegressBoxes, UpsampleLike, Anchors, ClipBoxes, RegressLines,ClipLines  # noqa: F401
 from .filter_detections import FilterDetections  # noqa: F401
--- keras_retinanet/layers/_smisc.py
+++ keras_retinanet/layers/_smisc.py
@@ -149,8 +149,54 @@ class RegressBoxes(keras.layers.Layer):
 
         return config
 
+class RegressLines(keras.layers.Layer):
+    """ Keras layer for applying regression values to boxes.
+    """
 
-class ClipBoxes(keras.layers.Layer):
+    def __init__(self, mean=None, std=None, *args, **kwargs):
+        """ Initializer for the RegressBoxes layer.
+
+        Args
+            mean: The mean value of the regression values which was used for normalization.
+            std: The standard value of the regression values which was used for normalization.
+        """
+        if mean is None:
+            mean = np.array([0, 0, 0])
+        if std is None:
+            std = np.array([0.2, 0.2, 0.2])
+
+        if isinstance(mean, (list, tuple)):
+            mean = np.array(mean)
+        elif not isinstance(mean, np.ndarray):
+            raise ValueError('Expected mean to be a np.ndarray, list or tuple. Received: {}'.format(type(mean)))
+
+        if isinstance(std, (list, tuple)):
+            std = np.array(std)
+        elif not isinstance(std, np.ndarray):
+            raise ValueError('Expected std to be a np.ndarray, list or tuple. Received: {}'.format(type(std)))
+
+        self.mean = mean
+        self.std  = std
+        super(RegressBoxes, self).__init__(*args, **kwargs)
+
+    def call(self, inputs, **kwargs):
+        anchors, regression = inputs
+        return backend.center_alpha_transform_inv(anchors, regression, mean=self.mean, std=self.std)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
+
+    def get_config(self):
+        config = super(RegressBoxes, self).get_config()
+        config.update({
+            'mean': self.mean.tolist(),
+            'std' : self.std.tolist(),
+        })
+
+        return config
+
+
+class ClipLines(keras.layers.Layer):
     """ Keras layer to clip box values to lie inside a given shape.
     """
     def call(self, inputs, **kwargs):
